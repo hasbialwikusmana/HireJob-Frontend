0a23a430cf39a5d0105726aa69f34bb926dbd088
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-02-08 17:24:19 +0700

    update router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Landing from "./pages/Landing/Landing";
+import Home from "./pages/home/Home";
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold ">Hello world!</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* LOGIN REGISTER */}
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/register" element={<Register />} />
+
+        {/* Landing */}
+        <Route path="/" element={<Landing />} />
+
+        {/* Home */}
+        <Route path="/home" element={<Home />} />
+
+        {/* NOT FOUND */}
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
